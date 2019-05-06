a4cc55a9f6b646b5cab1acff73c703e27987a262
Achmad Fatkharrofiqi <achmad@brainmatics.com>
2019-05-06 11:39:49 +0700

    [WIP] belajar mengkonekan data dari controller ke view

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -5,4 +5,20 @@ import { Component } from '@angular/core';
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {}
+export class Tab1Page {
+
+  myEmail = 'achmadfatkharrofiqi404@gmail.com';
+  myFav = {movie:'Aquaman',music:'Queen',sport:'Futsal'};
+  
+  myFavFruit = [
+    {id:1, name:'Apple'},
+    {id:2, name:'Blueberry'},
+    {id:3, name:'Chery'},
+    {id:4, name:'Orange'},
+    {id:5, name:'Manggo'},
+  ];
+
+  constructor(){
+
+  }
+}
